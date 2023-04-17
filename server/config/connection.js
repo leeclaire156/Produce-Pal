912b201a4e88faeea448913b2bff7b188fd25676
Claire Lee <lee.claire156@gmail.com>
2023-04-17 15:35:50 -0400

    Tests connection to MongoDB ATLAS

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,20 @@
 // This text is to allow GitHub to recognize this file and it parent folder's existence
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+try {
+    // added useNewUrlParser and useUnifiedTopology due to Deprecation Warnings
+    // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/localproducepal', {
+    // mongoose.connect(`${process.env.MONGO_URL}`, {
+    // mongoose.connect(`mongodb+srv://${process.env.ATLAS_ACCT}:${process.env.ATLAS_PWD}@${process.env.ATLAS_DB}.ad0wjug.mongodb.net/${process.env.ATLAS_DB_NAME}`, {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    // });
+    console.log(process.env.MONGODB_URI)
+    console.log(`mongodb+srv://${process.env.ATLAS_ACCT}:${process.env.ATLAS_PWD}@${process.env.ATLAS_DB}.ad0wjug.mongodb.net/${process.env.ATLAS_DB_NAME}`)
+} catch (err) {
+    console.log(err);
+}
+
+module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,48 @@
 // This text is to allow GitHub to recognize this file and it parent folder's existence
+const express = require('express');
+// const { ApolloServer } = require('apollo-server-express')
+const path = require('path');
+const db = require('./config/connection');
+// const { authMiddleware } = require('./utils/auth');
+// const { typeDefs, resolvers } = require('./schemas');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// const server = new ApolloServer({
+//     context: authMiddleware,
+//     typeDefs,
+//     resolvers
+// })
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })})
+
+// const startApolloServer = async (typeDefs, resolvers) => {
+//     await server.start();
+//     server.applyMiddleware({ app });
+
+//     db.once('open', () => {
+//         app.listen(PORT, () => {
+//             console.log(`API server running on port ${PORT}!`);
+//             console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//         })
+//     })
+// };
+
+// // Call the async function to start the server
+// startApolloServer(typeDefs, resolvers);
