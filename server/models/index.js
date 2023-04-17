eb712fe8bad0722b9e87ebc6bf224c6fb52dd7b3
MEwing <masandraewing@gmail.com>
2023-04-17 09:07:38 -0400

    Added Sharebox model to index for models folder

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,7 @@
 const Vendor = require('./Vendor');
 const Consumer = require('./Consumer');
 const Product = require('./Product');
+const Sharebox = require('./Sharebox');
 
-module.exports = { Vendor, Consumer, Product };
+module.exports = { Vendor, Consumer, Product, Sharebox };
 */
