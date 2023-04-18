1be0c9eb921ea67afb3c4a772bb3ca1a4741de02
MEwing <masandraewing@gmail.com>
2023-04-18 11:32:36 -0400

    updated order model

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,35 +1,51 @@
 const { Schema, model } = require('mongoose');
 
-const orderSchema = new Schema({
-    orderId: {
-        type: Number,
-        required: true, 
-    },
-    purchaseDate: {
-        type: Date,
-        default: Date.now,
-        required: true, 
-    },
-    orderContents: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Produce'
+const orderSchema = new Schema(
+    {
+        orderId: {
+            type: Number,
+            required: true,
         },
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Sharebox'
+        purchaseDate: {
+            type: Date,
+            default: Date.now,
+            required: true,
         },
-    ],
-    // For Order type: Pending, Paid, Ready, Closed
-    orderType: {
-        type: String,
-        required: true,
-        trim: true
+        orderContents: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Produce'
+            },
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Sharebox'
+            },
+        ],
+        // For Order type: Pending, Paid, Ready, Closed
+        orderType: {
+            type: String,
+            required: true,
+            trim: true
+        },
     },
-});
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
 
 // add virtual for the aggregate using $group
 
+// orderCount can be used for the shopping cart #?
+orderSchema.
+    virtual('orderCount')
+    .get(function () {
+    return this.orderContents.length;
+})
+
+// Initialize our Order model
 const Order = model('Order', orderSchema)
 
 module.exports = Order;
