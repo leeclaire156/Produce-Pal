2ce41c37ca871347912610f7094d245c200a559a
Jennilee Messenger <jennilee.messenger@gmail.com>
2023-04-24 11:39:12 -0400

    added files for stripe... All is commented out: Pages = Order History and Success, Client App.js and server.js

--- server/server.js
+++ server/server.js
@@ -29,29 +29,29 @@ app.use((req, res, next) => {
 // This is a public sample test API key.
 // Don’t submit any personally identifiable information in requests made with this key.
 // Sign in to see your own test API key embedded in code samples.
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+// const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
-const YOUR_DOMAIN = 'http://localhost:3000';
+// const YOUR_DOMAIN = 'http://localhost:3000';
 
-app.post('/create-checkout-session', async (req, res) => {
-    const session = await stripe.checkout.sessions.create({
-        line_items: [
-            {
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                price: '{{PRICE_ID}}',
-                quantity: 1,
-            },
-        ],
-        mode: 'payment',
-        success_url: `${YOUR_DOMAIN}?success=true`,
-        cancel_url: `${YOUR_DOMAIN}?canceled=true`,
-    });
+// app.post('/create-checkout-session', async (req, res) => {
+//     const session = await stripe.checkout.sessions.create({
+//         line_items: [
+//             {
+//                 // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+//                 price: '{{PRICE_ID}}',
+//                 quantity: 1,
+//             },
+//         ],
+//         mode: 'payment',
+//         success_url: `${YOUR_DOMAIN}?success=true`,
+//         cancel_url: `${YOUR_DOMAIN}?canceled=true`,
+//     });
 
-    res.redirect(303, session.url);
-});
+//     res.redirect(303, session.url);
+// });
 // -----------------------------------------------------------------------------------------
 
 
