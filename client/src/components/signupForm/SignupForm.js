7e040ed658ce0a05fa08b459c9290c17d1ed3262
Claire Lee <lee.claire156@gmail.com>
2023-04-23 21:58:50 -0400

    Removes consumerInfo Folder, replaced with userInfo folder.

--- client/src/components/signupForm/SignupForm.js
+++ client/src/components/signupForm/SignupForm.js
@@ -3,17 +3,18 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../../utils/auth';
 // import { ADD_USER } from '../utils/mutations';
-import { Form, Button, Container } from 'react-bootstrap';
+import { Form, Button, Dropdown } from 'react-bootstrap';
 import './signupForm.css';
 import eye from '../assets/eye_icongeek26.png'
 import blindeye from '../assets/blind-eye_icongeek26.png'
 import NavBar from '../NavBar';
+import axios from 'axios';
 
 function Signup(props) {
     const [formState, setFormState] = useState({ email: '', password: '' });
     const [errorEmailMessage, setEmailError] = useState(false);
     const [passwordType, setPasswordType] = useState(false)
-    const [image, setImage] = useState(true)
+    const [eyeImage, setEyeImage] = useState(true)
     const [notFilled, setFilledStatus] = useState(false)
     // const [addUser] = useMutation(ADD_USER);
 
@@ -80,8 +81,69 @@ function Signup(props) {
 
     const togglePassword = () => {
         setPasswordType(!passwordType)
-        setImage(!image)
+        setEyeImage(!eyeImage)
     }
+
+    const [loading, setLoading] = useState(false);
+    // const [url, setUrl] = useState("");
+
+    // const convertBase64 = (file) => {
+    //     return new Promise((resolve, reject) => {
+    //         const fileReader = new FileReader();
+    //         fileReader.readAsDataURL(file);
+
+    //         fileReader.onload = () => {
+    //             resolve(fileReader.result);
+    //         };
+
+    //         fileReader.onerror = (error) => {
+    //             reject(error);
+    //         };
+    //     });
+    // };
+
+    // function uploadSingleImage(base64) {
+    //     setLoading(true);
+    //     axios
+    //         .post("http://localhost:3000/uploadImage", { image: base64 })
+    //         .then((res) => {
+    //             setUrl(res.data);
+    //             alert(`Image uploaded Succesfully. Url is ${url}`);
+    //         })
+    //         .then(() => setLoading(false))
+    //         .catch(console.log);
+    // }
+
+    // function uploadMultipleImages(images) {
+    //     setLoading(true);
+    //     axios
+    //         .post("http://localhost:3000/uploadMultipleImages", { images })
+    //         .then((res) => {
+    //             setUrl(res.data);
+    //             alert("Image uploaded Succesfully");
+    //         })
+    //         .then(() => setLoading(false))
+    //         .catch(console.log);
+    // }
+
+    // const uploadImage = async (event) => {
+    //     const files = event.target.files;
+    //     console.log(files.length);
+
+    //     if (files.length === 1) {
+    //         const base64 = await convertBase64(files[0]);
+    //         uploadSingleImage(base64);
+    //         return;
+    //     }
+
+    //     const base64s = [];
+    //     for (var i = 0; i < files.length; i++) {
+    //         var base = await convertBase64(files[i]);
+    //         base64s.push(base);
+    //     }
+    //     uploadMultipleImages(base64s);
+    // };
+
 
     // const filledOutCheck = () => {
     //     if (!formState.firstName && !formState.lastName && !formState.email && !formState.password) {
@@ -127,6 +189,40 @@ function Signup(props) {
                     </Form.Group>
                     <Form.Group className="flex-row space-between my-2">
                         <Form.Control
+                            placeholder="Pick Up Address"
+                            name="pickupLocation"
+                            type="text"
+                            id="pickupLocation"
+                            onChange={handleChange}
+                        // value={formState.vendorAddress}
+                        />
+                        <Form.Control
+                            placeholder="City"
+                            name="pickupCity"
+                            type="text"
+                            id="pickupCity"
+                            onChange={handleChange}
+                        // value={formState.vendorAddress}
+                        />
+                        <Form.Control
+                            placeholder="State"
+                            name="pickupState"
+                            type="text"
+                            id="pickupState"
+                            onChange={handleChange}
+                        // value={formState.vendorAddress}
+                        />
+                        <Form.Control
+                            placeholder="Zip Code"
+                            name="pickupZipCode"
+                            type="text"
+                            id="pickupZipCode"
+                            onChange={handleChange}
+                        // value={formState.vendorAddress}
+                        />
+                    </Form.Group>
+                    <Form.Group className="flex-row space-between my-2">
+                        <Form.Control
                             placeholder="Business Phone Number"
                             name="vendorTelephone"
                             type="text"
@@ -140,6 +236,30 @@ function Signup(props) {
                         <Form.Control
                             placeholder="Business Address"
                             name="vendorAddress"
+                            type="text"
+                            id="vendorAddress"
+                            onChange={handleChange}
+                            value={formState.vendorAddress}
+                        />
+                        <Form.Control
+                            placeholder="City"
+                            name="vendorCity"
+                            type="text"
+                            id="vendorAddress"
+                            onChange={handleChange}
+                            value={formState.vendorAddress}
+                        />
+                        <Form.Control
+                            placeholder="State"
+                            name="vendorState"
+                            type="text"
+                            id="vendorAddress"
+                            onChange={handleChange}
+                            value={formState.vendorAddress}
+                        />
+                        <Form.Control
+                            placeholder="Zip Code"
+                            name="vendorZipCode"
                             type="text"
                             id="vendorAddress"
                             onChange={handleChange}
@@ -198,7 +318,7 @@ function Signup(props) {
                         value={formState.password}
                     />
                     <Button className="togglePwdBtn" onClick={togglePassword}>
-                        <img src={image ? eye : blindeye} />
+                        <img src={eyeImage ? eye : blindeye} />
                     </Button>
                 </Form.Group>
                 <Form.Group className="flex-row space-between my-2">
@@ -232,6 +352,20 @@ function Signup(props) {
                         value={formState.phone}
                     />
                 </Form.Group>
+                {/* <Form.Group>
+                    <Form.Label className='UploadImg mt-4 mb-5'>
+                        <div>
+                            Upload
+                        </div>
+                        <Form.Control
+                            id="dropzone-file"
+                            type="file"
+                            name='vendorImage'
+                            onChange={uploadImage}
+                            className='vendorImage'
+                        />
+                    </Form.Label>
+                </Form.Group> */}
                 <Form.Group className="flex-row space-between my-2">
                     <Form.Label htmlFor="vendorStatus">Are you a vendor?</Form.Label>
                     <Form.Check
@@ -251,14 +385,25 @@ function Signup(props) {
                 </Form.Group>
                 {renderVendorForm()}
                 <Form.Group className="flex-row flex-end">
-                    <Button
-                        id="signup-btn"
-                        className="signup-pg-btns btn-primary btn"
-                        type="submit"
+                    {loading ? (
+                        <Button
+                            id="signup-btn"
+                            className="signup-pg-btns btn-primary btn"
+                            type="submit"
+                            disabled
+                        >
+                            Register
+                        </Button>
+                    ) : (
+                        <Button
+                            id="signup-btn"
+                            className="signup-pg-btns btn-primary btn"
+                            type="submit"
                         // disabled={notFilled}
-                    >
-                        Register
-                    </Button>
+                        >
+                            Register
+                        </Button>
+                    )}
                     <Link to="/login">
                         <Button id="login-btn" className='signup-pg-btns btn-secondary btn'>Login</Button>
                     </Link>
