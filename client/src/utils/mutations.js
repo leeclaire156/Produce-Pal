f02f08f371d27e12a4809b06f43bf77148c3a37b
MEwing <masandraewing@gmail.com>
2023-04-24 22:37:59 -0400

    potential bug fix

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -31,10 +31,13 @@ export const LOGIN = gql`
   }
 `;
 
+// Note to Claire: for the addresses, can they add their addresses in their dashboard? Like have a button that then triggers 
+// the addAddress mutation for whatever address they want to edit? 
+
 export const ADD_USER = gql`
-mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!, $vendorStatus: Boolean!, $biography: String, $phone: String, $userImage: String, $vendorName: String, $vendorDescription: String, $marketName: String, $vendorTelephone: String, $vendorImage: String, $city: String!, $state: String!, $street: String!, $zipcode: String!, $addAddressEmail2: String!, $addVendorAddressCity2: String!, $addVendorAddressState2: String!, $addVendorAddressStreet2: String!, $addVendorAddressZipcode2: String!, $addVendorAddressEmail2: String!, $addPickupAddressCity2: String!, $addPickupAddressState2: String!, $addPickupAddressStreet2: String!, $addPickupAddressZipcode2: String!, $addPickupAddressEmail2: String!) {
-  addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password, vendorStatus: $vendorStatus, biography: $biography, phone: $phone, userImage: $userImage, vendorName: $vendorName, vendorDescription: $vendorDescription, marketName: $marketName, vendorTelephone: $vendorTelephone, vendorImage: $vendorImage) {
-    token
+mutation Mutation($firstName: String!, $lastName: String!, $email: String!, $password: String!, $vendorStatus: Boolean!) {
+  addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password, vendorStatus: $vendorStatus) {
+    token 
     user {
       _id
       firstName
@@ -69,26 +72,68 @@ mutation addUser($firstName: String!, $lastName: String!, $email: String!, $pass
       vendorImage
     }
   }
-  addAddress(city: $city, state: $state, street: $street, zipcode: $zipcode, email: $addAddressEmail2) {
-    street
-    city
-    state
-    zipcode
-  }
-  addVendorAddress(city: $addVendorAddressCity2, state: $addVendorAddressState2, street: $addVendorAddressStreet2, zipcode: $addVendorAddressZipcode2, email: $addVendorAddressEmail2) {
-    street
-    city
-    state
-    zipcode
-  }
-  addPickupAddress(city: $addPickupAddressCity2, state: $addPickupAddressState2, street: $addPickupAddressStreet2, zipcode: $addPickupAddressZipcode2, email: $addPickupAddressEmail2) {
-    street
-    city
-    state
-    zipcode
-  }
 }
 `;
+
+// export const ADD_USER = gql`
+// mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!, $vendorStatus: Boolean!, $biography: String, $phone: String, $userImage: String, $vendorName: String, $vendorDescription: String, $marketName: String, $vendorTelephone: String, $vendorImage: String, $city: String!, $state: String!, $street: String!, $zipcode: String!, $addAddressEmail2: String!, $addVendorAddressCity2: String!, $addVendorAddressState2: String!, $addVendorAddressStreet2: String!, $addVendorAddressZipcode2: String!, $addVendorAddressEmail2: String!, $addPickupAddressCity2: String!, $addPickupAddressState2: String!, $addPickupAddressStreet2: String!, $addPickupAddressZipcode2: String!, $addPickupAddressEmail2: String!) {
+//   addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password, vendorStatus: $vendorStatus, biography: $biography, phone: $phone, userImage: $userImage, vendorName: $vendorName, vendorDescription: $vendorDescription, marketName: $marketName, vendorTelephone: $vendorTelephone, vendorImage: $vendorImage) {
+//     token
+//     user {
+//       _id
+//       firstName
+//       lastName
+//       email
+//       address {
+//         street
+//         city
+//         state
+//         zipcode
+//       }
+//       biography
+//       phone
+//       userImage
+//       vendorStatus
+//       vendorName
+//       vendorDescription
+//       marketName
+//       pickupAddress {
+//         street
+//         city
+//         state
+//         zipcode
+//       }
+//       vendorTelephone
+//       vendorAddress {
+//         street
+//         city
+//         state
+//         zipcode
+//       }
+//       vendorImage
+//     }
+//   }
+//   addAddress(city: $city, state: $state, street: $street, zipcode: $zipcode, email: $addAddressEmail2) {
+//     street
+//     city
+//     state
+//     zipcode
+//   }
+//   addVendorAddress(city: $addVendorAddressCity2, state: $addVendorAddressState2, street: $addVendorAddressStreet2, zipcode: $addVendorAddressZipcode2, email: $addVendorAddressEmail2) {
+//     street
+//     city
+//     state
+//     zipcode
+//   }
+//   addPickupAddress(city: $addPickupAddressCity2, state: $addPickupAddressState2, street: $addPickupAddressStreet2, zipcode: $addPickupAddressZipcode2, email: $addPickupAddressEmail2) {
+//     street
+//     city
+//     state
+//     zipcode
+//   }
+// }
+// `;
+
 // for mutation above
 // {
 //   "firstName": "ASD",
