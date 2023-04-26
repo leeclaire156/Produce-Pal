7e6cecd7be58849a785828e30e15aae348d625b7
Claire Lee <lee.claire156@gmail.com>
2023-04-26 19:20:42 -0400

    Add order history link

--- client/src/components/VendorDashboard.js
+++ client/src/components/VendorDashboard.js
@@ -37,7 +37,9 @@ function VendorDashboard({ vendorName, vendorAddress, vendorDescription, email, 
                             <FontAwesomeIcon icon={faBook} size="3x" />
                             <h5 className="card-title mt-3">Consumer Orders</h5>
                             <p className="card-text">Review and manage consumer orders.</p>
-                            <button className="btn btn-primary">View</button>
+                            <Link to="/order-history">
+                                <button className="btn btn-primary">View</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
