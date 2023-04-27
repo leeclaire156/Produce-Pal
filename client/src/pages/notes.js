ab1936f2052c7961038835fd55b8f93579f125eb
MEwing <masandraewing@gmail.com>
2023-04-27 12:01:55 -0400

    old code commented out below grabbing data code

--- client/src/pages/notes.js
+++ client/src/pages/notes.js
@@ -1,12 +1,36 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { useProductContext } from '../utils/GlobalState';
+import {
+    UPDATE_PRODUCTS, UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY, TOGGLE_VENDOR_STATUS
+} from '../utils/actions';
+// productData needs to be hidden when merging
+// import productData from '../utils/products';
+import ProductSingleOther from '../components/ProductSingleOther';
+import Cart from '../components/Cart';
+import { idbPromise } from '../utils/helpers';
+import UserToggle from '../components/UserToggle';
+
+import axios from "axios";
+
+import { useMutation, useQuery } from '@apollo/client';
+import { ADD_PRODUCT } from '../utils/mutations';
+// import { QUERY_SINGLE_PROFILE, GET_ME } from '../utils/queries';
 import { STOREFRONT } from '../utils/queries';
 import { useParams } from 'react-router-dom';
 
-const { id } = useParams();
-console.log(id);
+const ProductInventoryOther = () => {
+    const { id } = useParams();
+    console.log(id);
+    
+    const { loading, error, data } = useQuery(STOREFRONT, {
+        variables: { id }
+    });
+    
+    console.log('Hi');
+    const storeData = data?.user || {};
+    console.log(storeData)
 
-const { loading, error, data } = useQuery(STOREFRONT, {
-    variables: { id }
-});
-
-const storeData = data?.user || {};
-console.log(storeData)
+    return(
+        <div>Hi</div>
+    )
