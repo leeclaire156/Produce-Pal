5c0857303d811e3c0fdbf2c71a5a2b1cfd35d632
Claire Lee <lee.claire156@gmail.com>
2023-04-29 11:52:24 -0400

    Changes maxAge syntax

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -23,7 +23,7 @@ module.exports = {
         // verify token and get user data out of it
         try {
             // const { data } = jwt.verify(token, secret, { maxAge: 10 });
-            const { data } = jwt.verify(token, secret, { maxAge: process.env.EXPIRATION });
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
             req.user = data;
         } catch {
             console.log('Invalid token');
