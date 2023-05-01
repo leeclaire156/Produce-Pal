1e9076fc5d6517e70af8a2775df0db154e9b8a1b
Claire Lee <lee.claire156@gmail.com>
2023-04-30 20:03:10 -0400

    This update fixes the issue where the JWT does not redirect when expired

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -22,7 +22,6 @@ module.exports = {
 
         // verify token and get user data out of it
         try {
-            // const { data } = jwt.verify(token, secret, { maxAge: 10 });
             const { data } = jwt.verify(token, secret, { maxAge: expiration });
             req.user = data;
         } catch {
